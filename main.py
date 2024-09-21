ee3d791474f946b9562434e8d347df622b1748e2
SpaceTurtle1226 <148325973+SpaceTurtle1226@users.noreply.github.com>
2024-09-21 15:47:34 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
+
+def draw_wimdow():
+    root = Tk()
+    label = Label(root, text="안녕세상")
+    label.pack()
+
+    root.mainloop()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    draw_wimdow()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
